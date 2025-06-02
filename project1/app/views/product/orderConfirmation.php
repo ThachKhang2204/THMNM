7d7dcfe67b4980b569657231c4e171721ce50b73
ThachKhang2204 <thachkhang2204@gmail.com>
2025-06-02 10:40:09 +0700

    Delete project1/app directory

--- project1/app/views/product/orderConfirmation.php
+++
@@ -1,7 +0,0 @@
-<?php include 'app/views/shared/header.php'; ?> 
-
-<h1>Xác nhận đơn hàng</h1> 
-<p>Cảm ơn bạn đã đặt hàng. Đơn hàng của bạn đã được xử lý thành công.</p> 
-<a href="/project1/Product" class="btn btn-primary mt-2">Tiếp tục mua sắm</a> 
-
-<?php include 'app/views/shared/footer.php'; ?>
